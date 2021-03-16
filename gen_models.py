eea0ee3841e19964ab73aef43f5b7a8223360f44
SilviaJIR <silvgong@gmail.com>
2021-03-15 20:51:00 -0700

    added genie_path

--- gen_models.py
+++ gen_models.py
@@ -8,7 +8,7 @@ from tqdm import tqdm
 
 from copy import deepcopy
 import torch_geometric.transforms as T
-from torch_geometric.nn import GCNConv, SAGEConv
+from torch_geometric.nn import GCNConv, SAGEConv, GATConv
 from torch_sparse import SparseTensor
 from torch_geometric.utils import to_undirected
 import numpy as np
@@ -90,13 +90,109 @@ class SGC(torch.nn.Module):
         return self.W(x)
 
 
-def train(model, x, y_true, train_idx, optimizer):
+class Breadth(torch.nn.Module):
+    def __init__(self, in_dim, out_dim, headnum):
+        super(Breadth, self).__init__()
+        self.gatconv = GATConv(in_dim, out_dim, heads=headnum)
+    def forward(self, x, edge_index):
+        x = torch.tanh(self.gatconv(x, edge_index))
+        return x
+
+class Depth(torch.nn.Module):
+    def __init__(self, in_dim, hidden):
+        super(Depth, self).__init__()
+        self.lstm = torch.nn.LSTM(in_dim, hidden, 1, bias=False)
+
+    def forward(self, x, h, c):
+        x, (h, c) = self.lstm(x, (h, c))
+        return x, (h, c)
+
+#todo:
+class GeniePathLayer(torch.nn.Module):
+    def __init__(self, in_dim, dim, headnum, lstm_hidden):
+        super(GeniePathLayer, self).__init__()
+        self.breadth_func = Breadth(in_dim, dim, headnum)
+        self.depth_func = Depth(dim, lstm_hidden)
+
+    def forward(self, x, edge_index, h, c):
+        x = self.breadth_func(x, edge_index)
+        x = x[None, :]
+        x, (h, c) = self.depth_func(x, h, c)
+        x = x[0]
+        return x, (h, c)
+
+
+class GeniePath(torch.nn.Module):
+    def __init__(self, in_dim, out_dim, head_num, lstm_hidden, num_layer, dim, residual_weight, device):
+        super(GeniePath, self).__init__()
+        self.lstm_hidden = lstm_hidden
+        self.device = device
+        self.lin1 = torch.nn.Linear(in_dim, dim)
+        self.gplayers = torch.nn.ModuleList([GeniePathLayer(dim, dim, head_num, lstm_hidden) for i in range(num_layer)])
+        self.lin2 = torch.nn.Linear(dim, out_dim)
+        self.residual_weight = residual_weight
+
+    def reset_parameters(self):
+        pass
+
+    def forward(self, x, edge_index):
+        x = self.lin1(x)
+        input = x
+        h = torch.zeros(1, x.shape[0], self.lstm_hidden).to(self.device)
+        c = torch.zeros(1, x.shape[0], self.lstm_hidden).to(self.device)
+        for i, l in enumerate(self.gplayers):
+            x, (h, c) = self.gplayers[i](x, edge_index, h, c)
+            #add skip layer
+            x = x+input*self.residual_weight
+
+        x = self.lin2(x)
+        return x
+
+
+class GeniePathLazy(torch.nn.Module):
+    def __init__(self, in_dim, out_dim, head_num, lstm_hidden, num_layer, dim, residual_weight, device):
+        super(GeniePathLazy, self).__init__()
+        self.device = device
+        self.lstm_hidden = lstm_hidden
+        self.lin1 = torch.nn.Linear(in_dim, dim)
+        #if breadth_model == 'GAT':
+        self.breaths = torch.nn.ModuleList([Breadth(dim, dim, head_num) for i in range(num_layer)])
+        # elif breadth_model == 'SGC':
+        #     self.breaths = torch.nn.ModuleList([Breadth(dim, dim, normalization, degree, device) for i in range(num_layer)])
+        self.depths = torch.nn.ModuleList([Depth(dim * 2, lstm_hidden) for i in range(num_layer)])
+        self.lin2 = torch.nn.Linear(dim, out_dim)
+        self.residual_weight = residual_weight
+
+    def reset_parameters(self):
+        pass
+
+    def forward(self, x, edge_index):
+        x = self.lin1(x)
+
+        #inp = x
+        h = torch.zeros(1, x.shape[0], self.lstm_hidden).to(self.device)
+        c = torch.zeros(1, x.shape[0], self.lstm_hidden).to(self.device)
+        h_tmps = []
+        for i, l in enumerate(self.breaths):
+            h_tmps.append(self.breaths[i](x, edge_index))
+        x = x[None, :]
+        inp = x
+        for i, l in enumerate(self.depths):
+            in_cat = torch.cat((h_tmps[i][None, :], x), -1)
+            x, (h, c) = self.depths[i](in_cat, h, c)
+            x = x+self.residual_weight*inp
+        x = self.lin2(x[0])
+        return F.log_softmax(x, dim=-1)
+
+def train(model, x, y_true, train_idx, optimizer, genie=False, adj_t=None):
     model.train()
 
     optimizer.zero_grad()
-    out = model(x[train_idx])
-    # loss = F.nll_loss(out, y_true.squeeze(1)[train_idx])
-    loss = F.cross_entropy(out, y_true.squeeze(1)[train_idx])
+    if genie:
+        out = model(x, adj_t)[train_idx]
+    else: out = model(x[train_idx])
+    loss = F.nll_loss(out, y_true.squeeze(1)[train_idx])
+    # loss = F.cross_entropy(out, y_true.squeeze(1)[train_idx])
 
     loss.backward()
     optimizer.step()
@@ -142,6 +238,11 @@ def main():
     parser.add_argument('--lr', type=float, default=0.01)
     parser.add_argument('--epochs', type=int, default=300)
     parser.add_argument('--runs', type=int, default=10)
+    parser.add_argument('--head_num', type=int, default=1)
+    parser.add_argument('--lstm_hidden', type=int, default=256)
+    parser.add_argument('--num_layer', type=int, default=4)
+    parser.add_argument('--genie_dim', type=int, default=256)
+    parser.add_argument('--residual_weight', type=float, default=0.2)
 
     args = parser.parse_args()
     print(args)
@@ -180,6 +281,8 @@ def main():
         model = MLPLinear(x.size(-1), dataset.num_classes).to(device)
     elif args.model=='sgc':
         model = SGC(x.size(-1), dataset.num_classes).to(device)
+    elif args.model=='genie':
+        model = GeniePathLazy(x.size(-1), dataset.num_classes, args.head_num, args.lstm_hidden, args.num_layer, args.genie_dim, args.residual_weight, args.device).to(device)
 
     x = x.to(device)
     y_true = data.y.to(device)
@@ -201,7 +304,7 @@ def main():
         best_valid = 0
         best_out = None
         for epoch in range(1, args.epochs):
-            loss = train(model, x, y_true, train_idx, optimizer)
+            loss = train(model, x, y_true, train_idx, optimizer, genie=args.model=='genie', adj_t=data.adj_t.to(device))
             result, out = test(model, x, y_true, split_idx, evaluator)
             train_acc, valid_acc, test_acc = result
             if valid_acc > best_valid:
